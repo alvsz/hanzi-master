8dd6e926941c9ff7999333fff0e6050ee6f74ddf
Joao Gabriel Alves <joao.aac@disroot.org>
2026-02-07 20:50:37 -0300

    pwa

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "hanzi-master-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "https://cdn.tailwindcss.com",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css",
+  "https://cdn.jsdelivr.net/npm/hanzi-writer@3.5/dist/hanzi-writer.min.js",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
